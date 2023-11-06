ce67e6b380b7d14f0e83b0c2262e21c401251f79
AaronParada <148873504+aaronparada1@users.noreply.github.com>
2023-11-06 19:24:48 +0100

    Update ej7.c
    Bug fixed

--- pr05/ej7.c
+++ pr05/ej7.c
@@ -1,24 +1,51 @@
 #include <stdio.h>
 #include <ctype.h>
 
-void label(char cadena[]),border(char n),menu();
+void label(char cadena[]),border(char n);
 int check(char cadena[]);
 int p_enroll(),p_filter(),p_list(),p_quit();
 
 
 int main(void){
 
-    int option;
+    int test;
+    char option;
     border('-');
     label("GAMER PROJECT");
     border('-');
 
-    do{
-        menu();
-        option=check("Are you sure you want con continue? (YN)");
-    }while (option!=0);
 
+    printf("E) Enrol\nQ) Quit\nL) List\nF) Filter\nX) Exit\n");
+    scanf(" %c", &option);
+    option = toupper(option);
+
+    switch (option) {
+        case 'E':
+            p_enroll();
+            break;
+
+        case 'Q':
+            p_quit();
+            break;
+
+        case 'L':
+            p_list();
+            break;
+
+        case 'F':
+            p_filter();
+            break;
+
+        case 'X':
+            do{
+                test=check("Are you sure you want con continue? (YN)\n");
+            }while(test != 0);
+            break;
+
+        default:
+            printf("Invalid option\n");
     return 0;
+    }
 }
 
 void label(char cadena[]){
@@ -52,51 +79,28 @@ void border(char n){
     printf("\n");
 }
 
-void menu() {
-    char option;
-    
-    do {
-        printf("E) Enrol\nQ) Quit\nL) List\nF) Filter\n");
-        scanf(" %c", &option);
-        option = toupper(option);
-
-        switch (option) {
-            case 'E':
-                p_enroll();
-                break;
-            case 'Q':
-                p_quit();
-                break;
-            case 'L':
-                p_list();
-                break;
-            case 'F':
-                p_filter();
-                break;
-            default:
-                printf("Invalid option\n");
-        }
-    } while (option != 'X');
-}
-
 int p_enroll(void){
     int value = 0;
+    printf("function enroll called value->%d\n", value);
     return value;
 }
 
 int p_quit(void){
     int value = 0;
+    printf("function quit called value ->%d\n", value);
     return value;
 }
 
 
 int p_list(void){
     int value = 0;
+    printf("function list called value->%d\n", value);
     return value;
 }
 
 int p_filter(void){
     int value = 0;
+    printf("function filter called value->%d\n", value);
     return value;
 }
 
